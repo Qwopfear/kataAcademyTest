2f99603209b4887a1bd19062fed811a32d420c74
Qwopfear <seva552.seva@gmail.com>
2022-06-21 21:39:13 -0700

    Done

--- src/Java/exception/NotMathOperationException.java
+++
@@ -1,7 +0,0 @@
-package Java.exception;
-
-public class NotMathOperationException extends Exception {
-    public NotMathOperationException(){
-        System.out.println("Is not math operation!!!");
-    }
-}
